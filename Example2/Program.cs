1b87dccda065d8cd3ff451578886d2359e9530f0
ShaMAD <at.shamad@gmail.com>
2022-12-02 17:28:59 +0300

    Change console.write

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Введите имя ");
 string username = Console.ReadLine();
-Console.Write("Hello, World ");
-Console.WriteLine(username);
+
+Console.Write($"Hello, World {username}\n");
